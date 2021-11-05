baa4153b8247ef1be23fb2deb244954a620c12ce
kenzi <a23g32@gmail.com>
2021-11-05 18:43:37 +0800

    add fiter

--- api/tourism.js
+++ api/tourism.js
@@ -2,8 +2,16 @@ import { fetcher } from "../lib/utils"
 
 export default {
 
-    getScenicSpotsByCity: async function (city) {
-        return await fetcher('/Tourism/ScenicSpot?$top=10&$format=JSON')
+    getScenicSpots: async function (city, name) {
+        let url = `/Tourism/ScenicSpot`
+
+        let filter = `?$top=10&$format=JSON`
+
+        if (city) url += `/${city}`
+        if (name) filter += `&$filter=contains(Name,${encodeURIComponent(`'${name}'`)})`
+
+        url += `${filter}`
+        return await fetcher(url, 'GET')
     }
 
 
