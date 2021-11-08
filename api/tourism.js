0e2149ca809a0a6df9a3d76e6ed3e1a1d51b8eb7
kenzi <a23g32@gmail.com>
2021-11-08 16:42:18 +0800

    added carousel

--- api/tourism.js
+++ api/tourism.js
@@ -1,18 +1,24 @@
 import { fetcher } from "../lib/utils"
 
 export default {
-
-    getScenicSpots: async function (city, name) {
+    getScenicSpots: async function (city, name, top = 10) {
         let url = `/Tourism/ScenicSpot`
 
-        let filter = `?$top=10&$format=JSON`
+        let filter = `?$top=${top}&$format=JSON`
 
         if (city) url += `/${city}`
         if (name) filter += `&$filter=contains(Name,${encodeURIComponent(`'${name}'`)})`
 
         url += `${filter}`
         return await fetcher(url, 'GET')
-    }
+    },
 
+    getActivities: async function (city, top = 10) {
+        let url = `/Tourism/Activity`
+        let filter = `?$top=${top}&$format=JSON`
+        if (city) url += `/${city}`
+        url += `${filter}`
+        return await fetcher(url, 'GET')
+    }
 
 }
--- components/Carousel.js
+++ components/Carousel.js
@@ -1,19 +1,44 @@
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
-
-
-export default (params) => {
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { LocationMarkerIcon } from '@heroicons/react/solid';
+import Image from 'next/image'
+export default ({ items }) => {
     return (
+
         <Carousel
-            plugins={[
-                'arrows',
-                'infinite'
-            ]}
+            autoPlay={true}
+            infiniteLoop
+            showThumbs={false}
+            showStatus={false}
         >
-            <div className=" w-[500px] h-[500px] bg-gray-300" ></div>
-            <div className=" w-[500px] h-[500px] bg-gray-400" ></div>
-            <div className=" w-[500px] h-[500px] bg-gray-500" ></div>
-            <div></div>
+
+            {items.map(item => (
+                <div className=" bg-gray-200">
+
+                    <div className=' relative w-full md:max-w-5xl  h-72 md:h-[500px]  mx-auto'>
+                        <Image
+                            src={item.Picture.PictureUrl1}
+                            layout="fill"
+                            objectFit="cover" />
+                        <div>
+
+                        </div>
+                        <div className=" absolute flex flex-row left-1 top-1 font-semibold text-xs text-white  items-center justify-between md:text-base md:left-2 md: top-2">
+                            <LocationMarkerIcon className=" h-3 md:h-4" />
+                            <span >{item.Location}</span>
+                            <span>{item.Name}</span>
+                        </div>
+
+                    </div>
+                </div>
+            ))}
+
+
+
+
+
+
+
         </Carousel>
     )
 }
