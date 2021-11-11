687af67a50fcffe1c4b87757149c18a0dd1aa301
kenzi <a23g32@gmail.com>
2021-11-11 13:44:45 +0800

    from next img to img

--- components/Carousel.js
+++ components/Carousel.js
@@ -15,15 +15,14 @@ export default ({ items }) => {
             {items.map(item => (
                 <div className=" bg-gray-200">
 
-                    <div className=' relative w-full md:max-w-5xl  h-72 md:h-[500px]  mx-auto'>
-                        <Image
+                    <div className=' relative w-full md:max-w-5xl  h-[35vh] md:h-[500px]  mx-auto'>
+                        <img
                             src={item.Picture.PictureUrl1}
-                            layout="fill"
-                            objectFit="cover" />
-                        <div>
-
-                        </div>
-                        <div className=" absolute flex flex-row left-1 top-1 font-semibold text-xs text-white  items-center justify-between md:text-base md:left-2 md: top-2">
+                            loading="lazy"
+                            alt={item.Name}
+                            className='min-h-full min-w-full max-h-full max-w-full'
+                        />
+                        <div className=" absolute flex flex-row left-1 top-1 font-semibold text-xs text-white  items-center justify-between md:text-base md:left-2 md:top-2 opacity-95">
                             <LocationMarkerIcon className=" h-3 md:h-4" />
                             <span >{item.Location}</span>
                             <span>{item.Name}</span>
